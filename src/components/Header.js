fc4d4eff29232bff76e380d56808578fdc1afb08
SGYSY <1476390950@qq.com>
2024-12-06 23:12:14 +0800

    change header and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,17 @@
-// src/components/Header.js
 import React from 'react';
+import './Header.css';
+import logoImage from '../picture/logo.png';
 
 function Header({ openAddModal }) {
   return (
-    <header style={headerStyle}>
-      <h1>数据库管理系统</h1>
+    <header className="header">
+      <div className="header-left">
+        <img src={logoImage} alt="Logo" className="logo" />
+        <span className="title">customer</span>
+      </div>
       <button className="search-btn">🔍</button>
-      <button style={buttonStyle} onClick={openAddModal}>添加记录</button>
     </header>
   );
 }
-
-const headerStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: '20px',
-  backgroundColor: '#f8f8f8',
-};
-
-const buttonStyle = {
-  padding: '8px 15px',
-  fontSize: '16px',
-  backgroundColor: '#4CAF50',
-  color: 'white',
-  border: 'none',
-  borderRadius: '5px',
-  cursor: 'pointer',
-};
 
 export default Header;
